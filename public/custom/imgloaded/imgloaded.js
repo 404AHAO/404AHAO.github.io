9240f5d5262b522c07694ba835dbc3261d207aa6
404AHAO <1600759690@qq.com>
2025-01-04 08:25:49 +0800

    Site updated: 2025-01-04 08:20:56

--- public/custom/imgloaded/imgloaded.js
+++ public/custom/imgloaded/imgloaded.js
@@ -1,1 +1,97 @@
-class ProgressiveLoad{constructor(e,a){this.smallSrc=e,this.largeSrc=a,this.initTpl()}initTpl(){this.container=document.createElement("div"),this.smallStage=document.createElement("div"),this.largeStage=document.createElement("div"),this.smallImg=new Image,this.largeImg=new Image,this.container.className="pl-container",this.smallStage.className="pl-img pl-blur",this.largeStage.className="pl-img",this.container.appendChild(this.smallStage),this.container.appendChild(this.largeStage),this.smallImg.onload=this._onSmallLoaded.bind(this),this.largeImg.onload=this._onLargeLoaded.bind(this)}progressiveLoad(){this.smallImg.src=this.smallSrc,this.largeImg.src=this.largeSrc}_onLargeLoaded(){this.largeStage.classList.add("pl-visible"),this.largeStage.style.backgroundImage=`url('${this.largeSrc}')`}_onSmallLoaded(){this.smallStage.classList.add("pl-visible"),this.smallStage.style.backgroundImage=`url('${this.smallSrc}')`}}const executeLoad=(e,a)=>{console.log("执行渐进背景替换");const t=window.matchMedia("(max-width: 767px)").matches,s=new ProgressiveLoad(t?e.mobileSmallSrc:e.smallSrc,t?e.mobileLargeSrc:e.largeSrc);a.children[0]&&a.insertBefore(s.container,a.children[0]),s.progressiveLoad()},config={smallSrc:"/img/xiaotu.jpg",largeSrc:"/img/tu.jpg",mobileSmallSrc:"/img/sjxt.jpg",mobileLargeSrc:"/img/sjdt.jpg",enableRoutes:["/"]};function initProgressiveLoad(e){const a=document.getElementById("page-header");a&&a.classList.contains("full_page")&&executeLoad(e,a)}function onPJAXComplete(e){const a=document.getElementById("page-header");a&&a.classList.contains("full_page")&&initProgressiveLoad(e)}document.addEventListener("DOMContentLoaded",(function(){initProgressiveLoad(config)})),document.addEventListener("pjax:complete",(function(){onPJAXComplete(config)}));
+// 首页头图加载优化
+/**
+ * @description 实现medium的渐进加载背景的效果
+ */
+class ProgressiveLoad {
+  constructor(smallSrc, largeSrc) {
+    this.smallSrc = smallSrc;
+    this.largeSrc = largeSrc;
+    this.initTpl();
+  }
+
+  /**
+   * @description 生成ui模板
+   */
+  initTpl() {
+    this.container = document.createElement('div');
+    this.smallStage = document.createElement('div');
+    this.largeStage = document.createElement('div');
+    this.smallImg = new Image();
+    this.largeImg = new Image();
+    this.container.className = 'pl-container';
+    this.smallStage.className = 'pl-img pl-blur';
+    this.largeStage.className = 'pl-img';
+    this.container.appendChild(this.smallStage);
+    this.container.appendChild(this.largeStage);
+    this.smallImg.onload = this._onSmallLoaded.bind(this);
+    this.largeImg.onload = this._onLargeLoaded.bind(this);
+  }
+
+  /**
+   * @description 加载背景
+   */
+  progressiveLoad() {
+    this.smallImg.src = this.smallSrc;
+    this.largeImg.src = this.largeSrc;
+  }
+
+  /**
+   * @description 大图加载完成
+   */
+  _onLargeLoaded() {
+    this.largeStage.classList.add('pl-visible');
+    this.largeStage.style.backgroundImage = `url('${this.largeSrc}')`;
+  }
+
+  /**
+   * @description 小图加载完成
+   */
+  _onSmallLoaded() {
+    this.smallStage.classList.add('pl-visible');
+    this.smallStage.style.backgroundImage = `url('${this.smallSrc}')`;
+  }
+}
+
+const executeLoad = (config, target) => {
+  console.log('执行渐进背景替换');
+  const isMobile = window.matchMedia('(max-width: 767px)').matches;
+  const loader = new ProgressiveLoad(
+    isMobile ? config.mobileSmallSrc : config.smallSrc,
+    isMobile ? config.mobileLargeSrc : config.largeSrc
+  );
+  // 和背景图颜色保持一致，防止高斯模糊后差异较大
+  if (target.children[0]) {
+    target.insertBefore(loader.container, target.children[0]);
+  }
+  loader.progressiveLoad();
+};
+
+const config = {
+  smallSrc: '/img/xiaotu.jpg', // 小图链接 尽可能配置小于100k的图片
+  largeSrc: '/img/tu.jpg', // 大图链接 最终显示的图片
+  mobileSmallSrc: '/img/sjxt.jpg', // 手机端小图链接 尽可能配置小于100k的图片
+  mobileLargeSrc: '/img/sjdt.jpg', // 手机端大图链接 最终显示的图片
+  enableRoutes: ['/'],
+  };
+
+function initProgressiveLoad(config) {
+  const target = document.getElementById('page-header');
+  if (target && target.classList.contains('full_page')) {
+    executeLoad(config, target);
+  }
+}
+
+function onPJAXComplete(config) {
+  const target = document.getElementById('page-header');
+  if (target && target.classList.contains('full_page')) {
+    initProgressiveLoad(config);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  initProgressiveLoad(config);
+});
+
+document.addEventListener("pjax:complete", function() {
+  onPJAXComplete(config);
+});
